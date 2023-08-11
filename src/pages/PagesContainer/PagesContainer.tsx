1991a716f5dd64e8aac40a41d1f9cf3e61c8fc0e
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-08-11 11:54:00 +0300

    upd PagesContainer, add menu switch

--- src/pages/PagesContainer/PagesContainer.tsx
+++ src/pages/PagesContainer/PagesContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react"
 import classNames from "classnames";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import { TabTypes } from "src/@types";
 import { FavoritesIcon, HomeIcon, SettingIcon, TrendsIcon } from "src/assets/icons";
@@ -9,6 +9,7 @@ import TabsList from "src/components/TabsList";
 import Footer from "src/components/Footer";
 
 import styles from './PagesContainer.module.scss';
+import { RoutesList } from "../Router";
 
 // так вот Outlet - это все то, что внутри подменяется (наши странички условно)
 // react самостоятельно контролит, какой компонент (странички) нужно менять/подтягивать по пути
@@ -16,13 +17,14 @@ const PagesContainer = () => {
 
     const [activeTab, setActiveTab] = useState(TabTypes.Home);
     const [isLoggedIn, setLoggedIn] = useState(false);
+    const { pathname } = useLocation();
 
     const tabsList = useMemo(
         () => [
-            { key: TabTypes.Home, title: 'Home', icon: <HomeIcon />, disabled: false },
-            { key: TabTypes.Trends, title: 'Trends', icon: <TrendsIcon />, disabled: false },
-            { key: TabTypes.Favourites, title: 'Favourites', icon: <FavoritesIcon />, disabled: !isLoggedIn },
-            { key: TabTypes.Settings, title: 'Settings', icon: <SettingIcon />, disabled: false }
+            { type: TabTypes.Home, title: 'Home', icon: <HomeIcon />, disabled: false, link: RoutesList.Home, },
+            { type: TabTypes.Trends, title: 'Trends', icon: <TrendsIcon />, disabled: false, link: RoutesList.Trends },
+            { type: TabTypes.Favourites, title: 'Favourites', icon: <FavoritesIcon />, disabled: !isLoggedIn, link: RoutesList.Favourites },
+            { type: TabTypes.Settings, title: 'Settings', icon: <SettingIcon />, disabled: false, link: RoutesList.MySettings }
         ],
         [isLoggedIn]
     );
@@ -49,12 +51,24 @@ const PagesContainer = () => {
 
             <div className={styles.containerContentSide}>
                 <div className={styles.tabsList}>
-                    <TabsList
-                        tabsList={tabsList}
-                        activeTab={activeTab}
-                        onTabClick={onTabClick}
-                    />
+                    {tabsList.map(({ link, type, title, icon, disabled }) => {
+                        return (
+                            <NavLink
+                                key={link}
+                                to={link}
+                                className={classNames(styles.tab, {
+                                    [styles.activeTab]: pathname === link,
+                                    [styles.disabledTab]: disabled === !isLoggedIn,
+                                    
+                                })}
+                            >
+                                {icon}
+                                {title}
+                            </NavLink>
+                        )
+                    })}
                 </div >
+
                 <div className={styles.footer}>
                     <Footer footer={'© All rights reserved'} className={styles.footerSecondary} />
                 </div>
