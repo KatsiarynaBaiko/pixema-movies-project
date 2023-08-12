78c108fd2cbf4ad665c25e38b9ae0cdbd3d218b2
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-08-12 13:20:46 +0300

    create rootSaga, connecting rootSaga to redax

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 
 import postReducer from './reducers/postSlice'
+import rootSaga from "./sagas/rootSaga";
+
+
+const sagaMiddleware = createSagaMiddleware ();
 
 const store = configureStore({
     reducer: {
         postReducer,
     },
+
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga); // подключение саги к редакс
 
 export type RootState = ReturnType<typeof store.getState>;
 
