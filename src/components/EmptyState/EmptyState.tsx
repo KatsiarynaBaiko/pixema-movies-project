f2429f3b2402fe89876827b8dd1e31d1201a1308
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-08-17 01:03:27 +0300

    add light theme in Empty State component, add EmptyState on Trends page

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from "react";
+import classNames from "classnames";
+import { Theme } from "src/@types";
+
 import { EmptyListIcon } from "src/assets/icons";
+import { useThemeContext } from "src/context/Theme";
+
 import styles from "./EmptyState.module.scss";
+
 
 type EmptyStatePropsType = {
   title: string;
@@ -9,12 +15,15 @@ type EmptyStatePropsType = {
 
 
 const EmptyState: FC<EmptyStatePropsType> = ({ title, description }) => {
+
+    const { themeValue } = useThemeContext();
+
   return (
     <div className={styles.container}>
       <EmptyListIcon />
       <div className={styles.infoContainer}>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.description}>{description}</div>
+        <div className={classNames(styles.title, {[styles.lightTitle] : themeValue === Theme.Light})}>{title}</div>
+        <div className={classNames(styles.description, {[styles.lightDescription] : themeValue === Theme.Light})}>{description}</div>
       </div>
     </div>
   );
--- src/pages/Trends/Trends.tsx
+++ src/pages/Trends/Trends.tsx
@@ -4,6 +4,7 @@ import { Theme } from "src/@types";
 import { useThemeContext } from "src/context/Theme";
 
 import styles from './Trends.module.scss';
+import EmptyState from "src/components/EmptyState";
 
 
 const Trends = () => {
@@ -12,7 +13,11 @@ const Trends = () => {
     
     return (
         <div className={styles.container}>
-            <div className={classNames(styles.comingSoon, {[styles.lightComingSoon] : themeValue === Theme.Light})}> Trends: Coming soon ...</div>
+            {/* <div className={classNames(styles.comingSoon, {[styles.lightComingSoon] : themeValue === Theme.Light})}> Trends: Coming soon ...</div> */}
+            <EmptyState
+            title={"Coming soon..."}
+            description={"Plese, visit another pages"}
+          />
         </div>
     )
 }
