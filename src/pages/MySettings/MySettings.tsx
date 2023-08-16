1fbc54d954c26817b7045bb45f7587ed0f2c27ba
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-08-16 17:58:25 +0300

    upd Settings pade: add input fields

--- src/pages/MySettings/MySettings.tsx
+++ src/pages/MySettings/MySettings.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 
 import styles from './MySettings.module.scss';
@@ -7,18 +7,48 @@ import ThemeSwitcher from "src/components/ThemeSwitcher";
 import { useThemeContext } from "src/context/Theme";
 import { Theme } from "src/@types";
 import Title from "src/components/Title";
+import Input from "src/components/Input";
 
 
 
 const MySettings = () => {
 
+    const { themeValue } = useThemeContext();
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+
     return (
         <div className={styles.container}>
+            
+            <Title className={classNames(styles.title)} title={"Profile"} />
+            <div className={classNames(styles.profileContainerInput, {[styles.lightProfileContainerInput] : themeValue === Theme.Light})}>
+                <Input title={'Name'} placeholder={'Your name'} onChange={setName} value={name} />
+                <Input title={'Email'} placeholder={'Your email'} onChange={setEmail} value={email} />
+            </div>
 
-            <div className={styles.title}><Title title={"Choose your Theme:"}/></div>
 
-            <ThemeSwitcher titleSun={"Use light thema"} titleMoon={"Use dark thema"} />
+            <div className={styles.passwordContainer}>
+                <Title className={styles.title} title={"Password"} />
+                <div className={classNames(styles.passwordContainerInput, {[styles.lightPasswordContainerInput] : themeValue === Theme.Light})}>
+                    <div className={styles.passwordContainerInputLeft}>
+                        <Input title={'Password'} placeholder={'Your password'} onChange={setPassword} value={password} />
+                    </div>
+                    <div className={styles.passwordContainerInputRight}>
+                        <Input title={'New Password'} placeholder={'New password'} onChange={setNewPassword} value={newPassword} />
+                        <Input title={'Confirm Password'} placeholder={'Confirm password'} onChange={setConfirmPassword} value={confirmPassword} />
+                    </div>
+                </div>
+            </div>
 
+            <Title className={styles.title} title={"Color mode"} />
+            <div className={styles.colorModeContainer}>
+                <ThemeSwitcher titleSun={"Use light thema"} titleMoon={"Use dark thema"} />
+            </div>
         </div>
     )
 }
