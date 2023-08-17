53512dd08932b97b435a1cd038288daba203f176
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-08-18 01:18:45 +0300

    use light theme for Pagination component, upd styles in this component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from "react";
 import ReactPaginate from "react-paginate";
+import classNames from "classnames";
+
+import { useThemeContext } from "src/context/Theme";
+import { Theme } from "src/@types";
 
 import styles from "./Pagination.module.scss";
-import classNames from "classnames";
 
 // type PaginationProps = {
 //   pagesCount: number;
@@ -43,10 +46,14 @@ type PaginationProps = {
   filmsPerPage: number;
   totalFilms: number;
   paginate: any;
+  className?: string;
 };
 
 
 const Paginate: FC<PaginationProps> = ({ filmsPerPage, totalFilms, paginate }) => {
+  
+  const { themeValue } = useThemeContext();
+
   const pageNumbers = []
 
   for (let i = 1; i <= Math.ceil(totalFilms / filmsPerPage); i++) {
@@ -55,15 +62,16 @@ const Paginate: FC<PaginationProps> = ({ filmsPerPage, totalFilms, paginate }) =
 
   return (
     <div>
-      <ul className="pagination">{
+      <ul className={styles.pagination}>{
         pageNumbers.map(number => (
-          <li className="page-item" key={number}>
-            <a href="#" className="page-link" onClick={() => paginate(number)}>
+          <li className={styles.pageItem} key={number}>
+            <a href="#" className={classNames(styles.pageLink, { [styles.lightPageLink]: themeValue === Theme.Light })} onClick={() => paginate(number)}>
               {number}
             </a>
           </li>
         ))
-      }</ul>
+      }
+      </ul>
     </div>
   );
 };
