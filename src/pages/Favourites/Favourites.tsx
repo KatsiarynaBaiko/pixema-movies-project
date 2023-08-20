7f2044bdcd486c7acfd01b91c229e9b8e2f3cdce
KatsiarynaBaiko <katerinabayko@gmail.com>
2023-08-20 20:01:03 +0300

    add EmptyState on Favourites page

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -8,6 +8,7 @@ import { useThemeContext } from "src/context/Theme";
 import { Theme } from "src/@types";
 
 import styles from './Favourites.module.scss';
+import EmptyState from "src/components/EmptyState";
 
 const Favourites = () => {
 
@@ -15,11 +16,21 @@ const Favourites = () => {
 
     const savedPosts = useSelector(PostSelectors.getSavedPosts);
     const isListLoading = useSelector(PostSelectors.getPostsListLoading)
-    
+
     return (
+
         <div className={styles.container}>
-            <div className={classNames(styles.comingSoon, {[styles.lightComingSoon] : themeValue === Theme.Light})}>Favourites: Coming soon ...</div>
-            <CardsList cardsList={savedPosts} isLoading={isListLoading}/>
+            {/* <div className={classNames(styles.comingSoon, {[styles.lightComingSoon] : themeValue === Theme.Light})}>Favourites: Coming soon ...</div> */}
+
+            {savedPosts.length ? (
+                <CardsList cardsList={savedPosts} isLoading={isListLoading} />
+            ) : (
+                <EmptyState
+                    title={"Nothing was found..."}
+                    description={"Please, visit anothe page and add to Favourites"}
+                />
+            )
+            }
         </div>
     )
 }
